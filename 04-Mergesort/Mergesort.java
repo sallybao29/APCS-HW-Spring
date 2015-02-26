1390d12169fb448a7ae8a8f86d482c269e647035
Sally Bao <sallybao29@gmail.com>
2015-02-25 22:05:05 -0500

    merge

---
+++ 04-Mergesort/Mergesort.java
@@ -0,0 +1,29 @@
+import java.util.*; 
+
+public class Mergesort{
+    public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
+	ArrayList<Integer> result = new ArrayList<Integer>();
+	while(a.size() > 0 || b.size() > 0){    
+	    if (a.size() <= 0){
+		result.add(b.remove(0));
+	    } 
+	    else  if (b.size() <= 0){
+		result.add(a.remove(0));
+	    }
+	    else if (a.get(0) > b.get(0)){
+		result.add(b.remove(0));
+	    }
+	    else {
+		result.add(a.remove(0));
+	    }
+	}
+	return result;
+    }
+
+    public static void main (String args[]){
+	Mergesort test = new Mergesort();
+	ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(1,2,5,37,47));
+	ArrayList<Integer> b = new ArrayList<Integer>(Arrays.asList(2,8,23));
+	System.out.println(test.merge(a, b));
+    }
+}
