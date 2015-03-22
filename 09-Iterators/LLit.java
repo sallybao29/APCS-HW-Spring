ed0dbf4e7b113b9f9199eea63c223a8d664cf775
Sally Bao <sallybao29@gmail.com>
2015-03-22 15:25:47 -0400

    remove

--- 09-Iterators/LLit.java
+++ 09-Iterators/LLit.java
@@ -2,10 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class LLit<E> implements Iterator<E>{
-    private Node<E> t, t2;
+    private Node<E> t, t2, first;
+    boolean removed;
+
     public LLit(Node<E> n){
+	first = n;
 	t = n;
 	t2 = n.getNext();
+	removed = false;
     }
 
     public boolean hasNext(){
@@ -13,14 +17,26 @@ public class LLit<E> implements Iterator<E>{
     }
 
     public E next(){
+	removed = false;
 	E retval = t2.getData();
 	t = t2;
-	t2 = t2.getNext();
+	t2 = t2.getNext();	
 	return retval;
     }
 
     public void remove(){
-        
+	if (removed == false){
+	    int i = 0;
+	    Node<E> tmp = first;
+	    while (tmp.getNext() != t){
+		tmp = tmp.getNext();
+	    }
+	    tmp.setNext(tmp.getNext().getNext());
+	    removed = true;
+	}
+	else {
+	    throw new IllegalStateException();
+	}
     }
 }
 
