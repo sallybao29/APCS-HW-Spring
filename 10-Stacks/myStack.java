3dc100a6f10818669ff7c913e9d20ad5af6c8953
Sally Bao <sallybao29@gmail.com>
2015-03-24 19:00:27 -0400

    stacks

---
+++ 10-Stacks/myStack.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+public class myStack<E>{
+    Node<E> l;
+ 
+    public myStack(){
+	l = new Node<E>();
+    }
+
+    public void push(E data){
+        Node<E> temp = new Node<E>(data);
+	temp.setNext(l.getNext());
+	l.setNext(temp);
+    }
+
+    public E pop(){
+	E temp = l.getNext().getData();
+	l.setNext(l.getNext().getNext());
+        return temp;
+    }
+
+    public boolean empty(){
+	return l.getNext() == null;
+    }
+
+    public E top() {
+        return l.getNext().getData();
+    }
+    public String toString(){
+	String result = "";
+	Node<E>temp = l.getNext();
+	while(temp != null){
+	    result = result + temp.getData() + "<---";
+	    temp = temp.getNext();
+	}
+	return result;
+    }
+    
+
+}
