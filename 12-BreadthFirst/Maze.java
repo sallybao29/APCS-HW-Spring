5ef274ff6579334d31a250f05ee1367172e115a2
sallybao29 <sallybao29@gmail.com>
2015-04-05 15:32:38 -0400

    Update Maze.java

--- 12-BreadthFirst/Maze.java
+++ 12-BreadthFirst/Maze.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Maze{  
     private char[][] board;
     private myQueue frontier;
-    private String current;
+    private Node current;
     private int maxX;
     private int maxY;
     private char processed = 'p';
@@ -53,15 +53,35 @@ public class Maze{
 	    }
 	return s;
     }
-    
-    /*
-      solved - instance variable to indicate we're done
-    */
+   
     public void solve(int x, int y){
-	frontier.enqueue(x, y);
-	while (!frontier.empty()){
-	    current = frontier.dequeue();
-	    board[current.substring(0,1)][current.substring(1,2)] = visited;
+	frontier = new myQueue(x, y);
+	while (!frontier.empty(){
+	    String c = frontier.dequeue();
+	    cX = current.substring(0,1);
+	    cY = current.substring(1,2);
+	    if (board[cX][cY] == exit){
+	    	solved = true;
+	    	return;
+	    }
+	    current = new Node(cX, cY);
+	    board[cX][cY] = visited;
+	    for (int dX = -1; dX < 2; dX++){
+	    	for (int dy = -1; dY < 2; dY++){
+	    		try{
+	    			if (board[cX+dX][cY+dY] == path &&
+	    			    board[cX+dX][cY+dY] != visited &&
+	    			    board[cX+dX][cY+dY] != processed){
+	    			    	Node n = new Node(cX+dX, cY+dY);
+	    			    	frontier.enqueue(n);
+	    			    	n.setPrevious(current);
+	    			    	board[cX+dX][cY+dY] = processed;
+	    		
+	    			    }
+	    		}
+	    		catch (IndexOutofBoundsException e){}
+	    	}
+	    }
 	    
 	}
     }
